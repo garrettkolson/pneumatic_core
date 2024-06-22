8f3d5449ce12382ab575af0b6a011797cf11d5ea
garrettkolson <garrett@smallwerks.com>
2024-06-21 22:26:41 -0600

    added ConnFactory and related traits/structs

--- src/conns.rs
+++ src/conns.rs
@@ -1,5 +1,38 @@
+use std::io::Write;
+use std::net::{TcpStream, ToSocketAddrs};
+
 pub trait IsConnection : Send {
 
+}
+
+pub trait FireAndForgetSender {
+    fn send<T>(&self, addr: T, data: Vec<u8>) where T : ToSocketAddrs;
+}
+
+pub struct TcpFafSender {
+
+}
+
+impl FireAndForgetSender for TcpFafSender {
+    fn send<T>(&self, addr: T, data: Vec<u8>) where T: ToSocketAddrs {
+        if let Ok(mut stream) = TcpStream::connect(addr) {
+            let _ = stream.write_all(&*data);
+        }
+    }
+}
+
+pub trait ConnFactory {
+    fn get_faf_sender() -> dyn FireAndForgetSender;
+}
+
+pub struct TcpConnFactory {
+
+}
+
+impl ConnFactory for TcpConnFactory {
+    fn get_faf_sender() -> TcpFafSender {
+        TcpFafSender {}
+    }
 }
 
 pub const HEARTBEAT_PORT: u16 = 44000;
