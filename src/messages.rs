09090f4dfd09fccddc7180db0066c7942a9f5b23
garrettkolson <garrett@smallwerks.com>
2024-07-13 21:40:20 -0600

    renamed Message.chain_id to Message.env_id to match naming elsewhere

--- src/messages.rs
+++ src/messages.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Message {
-    pub chain_id: String,
+    pub env_id: String,
     pub action: String,
     pub body: Vec<u8>,
     pub signature: Vec<u8>,
