74c5c8c90ef01b1074af8e1995055208cd82deb8
garrettkolson <garrett@smallwerks.com>
2024-07-14 22:21:43 -0600

    removed unnecessary Arc<Mutex>> from FileLogger impl

--- src/logging.rs
+++ src/logging.rs
@@ -1,5 +1,4 @@
 use std::io::Write;
-use std::sync::{Arc, Mutex};
 use file_lock::{FileLock, FileOptions};
 
 pub trait Logger {
@@ -8,30 +7,22 @@ pub trait Logger {
 
 pub struct FileLogger {
     file_path: String,
-    file_access: Arc<Mutex<Box<dyn FnMut(&String, String)>>>
 }
 
 impl FileLogger {
     pub fn new(file_name: String) -> Self {
         FileLogger {
-            file_path: file_name,
-            file_access: Arc::new(Mutex::new(Box::new(|path, mes| {
-                Self::log_message(path, mes)
-            })))
+            file_path: file_name
         }
-    }
-
-    fn log_message(path: &str, message: String) {
-        let options = FileOptions::new().write(true).append(true).create(true);
-
-        let Ok(mut file_lock) = FileLock::lock(path, true, options) else { return };
-        let _ = file_lock.file.write_all(message.as_bytes());
     }
 }
 
 impl Logger for FileLogger {
     fn log(&self, message: String) {
-        let Ok(mut func) = self.file_access.lock() else { return };
-        func(&self.file_path, message)
+        let options = FileOptions::new().write(true).append(true).create(true);
+
+        let Ok(mut file_lock) = FileLock::lock(&self.file_path, true, options) else { return };
+        let log_entry = format!("{0}: {1}", chrono::Utc::now(), message);
+        let _ = file_lock.file.write_all(log_entry.as_bytes());
     }
 }
