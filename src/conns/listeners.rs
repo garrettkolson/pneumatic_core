eca02a4cead6621c3c8e7f696fa97aceb8f86383
garrettkolson <garrett@smallwerks.com>
2025-04-23 11:56:32 -0600

    CoreUdsListener impl WIP

--- src/conns/listeners.rs
+++ src/conns/listeners.rs
@@ -1,8 +1,31 @@
+use std::io::Error;
 use std::net::{SocketAddr, TcpListener};
-use crate::conns::streams::{CoreTcpStream, Stream};
+use std::os::unix::net::UnixListener;
+use crate::conns::streams::{CoreTcpStream, CoreUdsStream, Stream};
 
 pub trait Listener {
     fn accept(&self) -> Result<(Box<dyn Stream>, SocketAddr), std::io::Error>;
+}
+
+pub struct CoreUdsListener {
+    inner_listener: UnixListener
+}
+
+impl CoreUdsListener {
+    pub(crate) fn new(addr: std::os::unix::net::SocketAddr) -> Self {
+        CoreUdsListener {
+            inner_listener: UnixListener::bind_addr(&addr)
+                .expect(&format!("Couldn't set up internal UDS listener on socket {0}", addr.as_pathname()))
+        }
+    }
+}
+
+impl Listener for CoreUdsListener {
+    fn accept(&self) -> Result<(Box<dyn Stream>, SocketAddr), Error> {
+        let (stream, addr) = self.inner_listener.accept()?;
+        let core_stream = Box::new(CoreUdsStream::from_stream(stream));
+        Ok((core_stream, addr))
+    }
 }
 
 pub struct CoreTcpListener {
